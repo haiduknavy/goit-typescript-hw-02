8a2948ddbe9059bc419d864ad286eea425708c3e
haiduknavy <79574457+haiduknavy@users.noreply.github.com>
2023-11-07 11:14:19 +0200

    End HW

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,20 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
+interface Props {
+  title: string;
+}
 
+class Component<T> {
+  props: T;
+
+  constructor(props: T) {
+    this.props = props;
   }
 }
 
-class Page extends Component {
-  pageInfo () {
+class Page extends Component<Props> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
